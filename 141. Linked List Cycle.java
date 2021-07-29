dd2636e2da21f507a430013c961f83cf22b85111
harshitsingh29 <harsh.official29@gmail.com>
2021-07-29 20:53:16 +0530

    Create 141. Linked List Cycle.java

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,25 @@
+// link - https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode first = head;
+        ListNode second = head;
+        while(first != null && first.next != null) {
+            first = first.next.next;
+            second = second.next;
+            if(first == second) return true;
+        }
+        return false;
+    }
+}
